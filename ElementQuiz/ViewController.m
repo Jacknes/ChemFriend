449117ec49e76931ef3390ea588a2a4ab58c8ff4
Jack Pascoe <Jacknes@s-jack-pascoe.local>
2015-07-01 00:42:13 +1000

    Added Basic answer selection and checking - also added randomisation of question selection.

--- ElementQuiz/ViewController.m
+++ ElementQuiz/ViewController.m
@@ -9,19 +9,97 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(strong, nonatomic) NSArray * symbols;
+@property(strong, nonatomic) NSArray * elements;
+@property(strong, nonatomic) NSString * answer;
+@property(strong, nonatomic) NSNumber * score;
+//@property(strong, nonatomic) NSNumber * rand;
+@property (weak, nonatomic) IBOutlet UIButton *option1;
+@property (weak, nonatomic) IBOutlet UIButton *option2;
+@property (weak, nonatomic) IBOutlet UIButton *option3;
+@property (weak, nonatomic) IBOutlet UIButton *option4;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *elementMain;
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+ //   NSArray * symbols = [@"H", @"He"];
+    
+    
+    
+    
+    self.symbols = @[@"H", @"He", @"Li", @"Be", @"B", @"C", @"N"];
+    
+    [self generateQuestions:(nil)];
+    
+    
+    
+  /*  //self.option1.titleLabel.text = self.symbols[0];
+    [self.option1 setTitle:self.symbols[0] forState:UIControlStateNormal];
+    [self.option2 setTitle:self.symbols[1] forState:UIControlStateNormal];
+    [self.option3 setTitle:self.symbols[2] forState:UIControlStateNormal];
+   */
+    
+    
+    
+    
+}
+- (IBAction)buttonPress:(id)sender
+{
+    NSString *selection = [(UIButton *)sender currentTitle];
+    self.scoreLabel.text = selection;
+    
+    if ([selection isEqualToString:self.answer])
+    {
+     self.scoreLabel.text = @"+1 xdd";
+     //self.score = (self.score + 1)
+        
+    // Make it pop up message here.
+        
+    }
+    
+}
+
+
+
+
+- (void)generateQuestions:(id)sender
+{
+    
+    // Selects a random element
+    int randomQuestion = arc4random_uniform(6);
+    // Selects a random button
+    int randomButton = arc4random_uniform(3);
+    //Sets the correct answer
+    self.answer = self.symbols[randomQuestion];
+    //[self.buttons(randomButton) setTitle:self.symbols[randomQuestion] forState:UIControlStateNormal];
+
+    
+    for (int i = 0; i < 3; i++)
+    {
+        int rands = arc4random_uniform(3);
+        [self.option1 setTitle:self.symbols[0] forState:UIControlStateNormal];
+        [self.option2 setTitle:self.symbols[1] forState:UIControlStateNormal];
+        [self.option3 setTitle:self.symbols[2] forState:UIControlStateNormal];
+        [self.option4 setTitle:self.symbols[randomQuestion] forState:UIControlStateNormal];
+        
+       
+    }
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
