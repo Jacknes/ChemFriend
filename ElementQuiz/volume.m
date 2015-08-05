25db815e5e9cf5eba0b2b20d622fcf742ee445b3
Jack Pascoe <Jacknes@s-jack-pascoe.local>
2015-08-05 16:36:39 +1000

    - Scenario textview added to volume page to help clarify what the values they are receiving mean. - Ability to resign keyboard on volume page.

--- ElementQuiz/volume.m
+++ ElementQuiz/volume.m
@@ -17,6 +17,10 @@
 @property (weak, nonatomic) IBOutlet UITextField *resultText;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *unitSelect;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *conditionSelect;
+@property (weak, nonatomic) IBOutlet UITextView *scenarioText;
+
+@property(strong, nonatomic) NSString * conditionStr;
+@property(strong, nonatomic) NSString * resultStr;
 
 
 @end
@@ -26,13 +30,16 @@
 @implementation volume
 
 float condition = 24.79; // Because the segment display is automatically on RTP
-float result = 0.0;
+
+float result = 0.0; // Initialises a starting value for result. 
+
 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.conditionStr = @"room temperature and pressure (25 Degrees, 298K)"; // Sets the conditionstr initially as the user may not change the state.
 }
 
 // STP - 22.71L/mol
@@ -49,10 +56,18 @@ float result = 0.0;
         value = [self.valueText.text floatValue];
         
         result = (value/condition);
+        self.resultStr = [NSString stringWithFormat:@"%f", result];
+        
+        
         
         self.resultText.text = [NSString stringWithFormat:@"%f", result];
         self.resultLabel.text = @"Moles";
         
+        self.scenarioText.text =  [NSString stringWithFormat:@"%s%@%s%@", "The number of moles in your volume of gas at ", self.conditionStr, " is ", self.resultStr];
+        
+      /*  [NSString stringWithFormat:@"%s", "You're volume of gas at " + self.conditionStr + "is" + self.resultStr]; */
+        
+       
         
     }
     
@@ -62,34 +77,34 @@ float result = 0.0;
             
             value = [self.valueText.text floatValue];
             result = (value * condition);
-            
+            self.resultStr = [NSString stringWithFormat:@"%f", result];
             self.resultText.text = [NSString stringWithFormat:@"%f", result];
             self.resultLabel.text = @"Volume";
+            self.scenarioText.text =  [NSString stringWithFormat:@"%s%@%s%@", "The number of litres of your gas at ", self.conditionStr, " is ", self.resultStr];
         
     }
     
-   
     
-    
-
-    
-
-    
-    
+    [self.valueText resignFirstResponder];
 
     
 }
 
+- (IBAction)resignKeyboard:(id)sender {
+    [self.valueText resignFirstResponder];
+}
 
 - (IBAction)conditionChanged:(id)sender
 {
     if ([self.conditionSelect selectedSegmentIndex] == 1) {
         condition = 22.71; // Sets the condition as STP
+        self.conditionStr = @"standard temperature and pressure (0 degrees, 273K)";
         
     }
     
     else if ([self.conditionSelect selectedSegmentIndex] == 0) {
         condition = 24.79; // Sets the condition as RTP
+        self.conditionStr = @"room temperature and pressure (25 Degrees, 298K)";
     }
     
 }
