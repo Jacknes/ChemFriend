d7b234b77e5fbf0c2a44ac38436db885dd07c7e8
Jack Pascoe <Jacknes@s-jack-pascoe.local>
2015-08-10 20:55:52 +1000

    - Primary functionality added to the galvanic cell page - output of both the cathode, anode and voltage. - Layout of all pages changed to add to consistency

--- ElementQuiz/galvanicCell.m
+++ ElementQuiz/galvanicCell.m
@@ -15,6 +15,12 @@
 @property (weak, nonatomic) IBOutlet UITextField *metal2Text;
 @property (weak, nonatomic) IBOutlet UITextView *informationText;
 @property(strong, nonatomic) NSArray* elements;
+@property(strong, nonatomic) NSDictionary * voltages;
+@property (weak, nonatomic) IBOutlet UITextField *cathodeText;
+@property (weak, nonatomic) IBOutlet UITextField *anodeText;
+@property (weak, nonatomic) IBOutlet UITextField *voltageText;
+
+
 
 @end
 
@@ -28,6 +34,10 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.elements = @[@"potassium", @"barium", @"calcium", @"sodium", @"magnesium", @"aluminium", @"manganese", @"zinc", @"iron II", @"nickel", @"tin", @"lead", @"hydrogen", @"copper II", @"copper", @"iodine", @"iron III", @"silver", @"bromine", @"chlorine", @"fluorine"]; // Creates the array of elements used to determine cathode/anode
+
+    NSString * path = [[NSBundle mainBundle] pathForResource:@"voltages" ofType:@"plist" inDirectory:nil];
+    self.voltages = [NSDictionary dictionaryWithContentsOfFile:path];
+    
 }
 
 - (IBAction)calculatePress:(id)sender {
@@ -35,6 +45,10 @@
     
     NSString *metal1 = self.metal1Text.text;
     NSString *metal2 = self.metal2Text.text;
+    NSNumber *volt1;
+    NSNumber *volt2;
+    NSNumber *voltage;
+    
     int location1 = 0;
     int location2 = 0;
     
@@ -58,16 +72,37 @@
     }
     
     if (location1 > location2){
-        self.informationText.text = @"lol metal 2 is anode";
+        self.cathodeText.text = metal1;
+        self.anodeText.text = metal2;
+        volt1 = [self.voltages objectForKey:metal1];
+        
+        volt2 = [self.voltages objectForKey:metal2];
+        voltage = @([volt1 floatValue] - [volt2 floatValue]);
+        
+      
+        
+        self.voltageText.text = [NSString stringWithFormat:@"%@", voltage];
+        // NSNumber *sum = @([first integerValue] + [second integerValue]);
+        // [NSString stringWithFormat:@"%@%d%@%d", part1,num1, part2, num2];
+        
+        
+        
     }
     
     else if (location1 < location2){
-        self.informationText.text = @"lol metal 1 is anode";
+        self.cathodeText.text = metal2;
+        self.anodeText.text = metal1;
+        volt1 = [self.voltages objectForKey:metal1];
+       // Need to make one neg
+        volt2 = [self.voltages objectForKey:metal2];
+        voltage = @([volt2 floatValue] - [volt1 floatValue]);
+        self.voltageText.text = [NSString stringWithFormat:@"%@", voltage];
     }
     
     else {
-        self.informationText.text = @"The values are equal -_-";
+        self.voltageText.text = @"The values are equal -_-";
     }
+    
 }
 
 
